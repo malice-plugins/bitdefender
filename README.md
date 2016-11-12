8b0939ee1e2b9ee44c5998c5c925a9bf47b06702
blacktop <blacktop@users.noreply.github.com>
2016-11-12 15:12:15 -0700

    add files

--- README.md
+++ README.md
@@ -1,2 +1,139 @@
-# malice-bitdefender
+malice-bitdefender
+==================
+
+[![Circle CI](https://circleci.com/gh/maliceio/malice-bitdefender.png?style=shield)](https://circleci.com/gh/maliceio/malice-bitdefender)
+[![License](http://img.shields.io/:license-mit-blue.svg)](http://doge.mit-license.org)
+[![Docker Stars](https://img.shields.io/docker/stars/malice/bitdefender.svg)](https://hub.docker.com/r/malice/bitdefender/)
+[![Docker Pulls](https://img.shields.io/docker/pulls/malice/bitdefender.svg)](https://hub.docker.com/r/malice/bitdefender/)
+[![Docker Image](https://img.shields.io/badge/docker image--blue.svg)](https://hub.docker.com/r/malice/bitdefender/)
+
+This repository contains a **Dockerfile** of [Bitdefender](http://www.bitdefender.com/business/antivirus-for-unices.html) for [Docker](https://www.docker.io/)'s [trusted build](https://hub.docker.com/r/malice/bitdefender/) published to the public [DockerHub](https://hub.docker.com).
+
+### Dependencies
+
+-	[ubuntu:precise (*138 MB*\)](https://hub.docker.com/_/ubuntu/)
+
+### Installation
+
+1.	Install [Docker](https://www.docker.io/).
+2.	Download [trusted build](https://hub.docker.com/r/malice/bitdefender/) from public [DockerHub](https://hub.docker.com): `docker pull malice/bitdefender`
+
+### Usage
+
+```
+docker run --rm malice/bitdefender EICAR
+```
+
+#### Or link your own malware folder:
+
+```bash
+$ docker run --rm -v /path/to/malware:/malware:ro malice/bitdefender FILE
+
+Usage: bitdefender [OPTIONS] COMMAND [arg...]
+
 Malice Bitdefender AntiVirus Plugin
+
+Version: v0.1.0, BuildTime: 20160227
+
+Author:
+  blacktop - <https://github.com/blacktop>
+
+Options:
+  --verbose, -V         verbose output
+  --table, -t           output as Markdown table
+  --post, -p            POST results to Malice webhook [$MALICE_ENDPOINT]
+  --proxy, -x           proxy settings for Malice webhook endpoint [$MALICE_PROXY]
+  --timeout value       malice plugin timeout (in seconds) (default: 10) [$MALICE_TIMEOUT]  
+  --elasitcsearch value elasitcsearch address for Malice to store results [$MALICE_ELASTICSEARCH]
+  --help, -h            show help
+  --version, -v         print the version
+
+Commands:
+  update        Update virus definitions
+  help          Shows a list of commands or help for one command
+
+Run 'bitdefender COMMAND --help' for more information on a command.
+```
+
+This will output to stdout and POST to malice results API webhook endpoint.
+
+## Sample Output
+
+### JSON:
+
+```json
+{
+  "bitdefender": {
+    "infected": true,
+    "result": "EICAR-Test-File (not a virus)",
+    "engine": "7.90123",
+    "updated": "20160227"
+  }
+}
+```
+
+### STDOUT (Markdown Table):
+
+---
+
+#### Bitdefender
+
+| Infected | Result                        | Engine  | Updated  |
+|----------|-------------------------------|---------|----------|
+| true     | EICAR-Test-File (not a virus) | 7.90123 | 20160227 |
+
+---
+
+Documentation
+-------------
+
+### To write results to [ElasticSearch](https://www.elastic.co/products/elasticsearch)
+
+```bash
+$ docker volume create --name malice
+$ docker run -d --name elastic \
+                -p 9200:9200 \
+                -v malice:/usr/share/elasticsearch/data \
+                 blacktop/elasticsearch
+$ docker run --rm -v /path/to/malware:/malware:ro --link elastic malice/bitdefender -t FILE
+```
+
+### POST results to a webhook
+
+```bash
+$ docker run -v `pwd`:/malware:ro \
+             -e MALICE_ENDPOINT="https://malice.io:31337/scan/file" \
+             malice/bitdefender --post evil.malware
+```
+
+### To update the AV run the following:
+
+```bash
+$ docker run --name=bitdefender malice/bitdefender update
+```
+
+Then to use the updated bitdefender container:
+
+```bash
+$ docker commit bitdefender malice/bitdefender:updated
+$ docker rm bitdefender # clean up updated container
+$ docker run --rm malice/bitdefender:updated EICAR
+```
+
+### Issues
+
+Find a bug? Want more features? Find something missing in the documentation? Let me know! Please don't hesitate to [file an issue](https://github.com/maliceio/malice-bitdefender/issues/new).
+
+### CHANGELOG
+
+See [`CHANGELOG.md`](https://github.com/maliceio/malice-bitdefender/blob/master/CHANGELOG.md)
+
+### Contributing
+
+[See all contributors on GitHub](https://github.com/maliceio/malice-bitdefender/graphs/contributors).
+
+Please update the [CHANGELOG.md](https://github.com/maliceio/malice-bitdefender/blob/master/CHANGELOG.md) and submit a [Pull Request on GitHub](https://help.github.com/articles/using-pull-requests/).
+
+### License
+
+MIT Copyright (c) 2016 **blacktop**
---
+++ scan.go
@@ -0,0 +1,265 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/crackcomm/go-clitable"
+	"github.com/fatih/structs"
+	"github.com/maliceio/go-plugin-utils/database/elasticsearch"
+	"github.com/maliceio/go-plugin-utils/utils"
+	"github.com/parnurzeal/gorequest"
+	"github.com/urfave/cli"
+)
+
+// Version stores the plugin's version
+var Version string
+
+// BuildTime stores the plugin's build time
+var BuildTime string
+
+const (
+	name     = "bitdefender"
+	category = "av"
+)
+
+type pluginResults struct {
+	ID   string      `json:"id" structs:"id,omitempty"`
+	Data ResultsData `json:"bitdefender" structs:"bitdefender"`
+}
+
+// Bitdefender json object
+type Bitdefender struct {
+	Results ResultsData `json:"bitdefender"`
+}
+
+// ResultsData json object
+type ResultsData struct {
+	Infected bool   `json:"infected" structs:"infected"`
+	Result   string `json:"result" structs:"result"`
+	Engine   string `json:"engine" structs:"engine"`
+	Updated  string `json:"updated" structs:"updated"`
+}
+
+// ParseBitdefenderOutput convert bitdefender output into ResultsData struct
+func ParseBitdefenderOutput(bitdefenderout string, err error) ResultsData {
+
+	if err != nil {
+		return ResultsData{}
+	}
+
+	bitdefender := ResultsData{Infected: false}
+	// EXAMPLE OUTPUT:
+	// BitDefender Antivirus Scanner for Unices v7.90123 Linux-amd64
+	// Copyright (C) 1996-2009 BitDefender. All rights reserved.
+	// Trial key found. 30 days remaining.
+	//
+	// Infected file action: ignore
+	// Suspected file action: ignore
+	// Loading plugins, please wait
+	// Plugins loaded.
+	//
+	// /malware/EICAR  infected: EICAR-Test-File (not a virus)
+	//
+	//
+	// Results:
+	// Folders: 0
+	// Files: 1
+	// Packed: 0
+	// Archives: 0
+	// Infected files: 1
+	// Suspect files: 0
+	// Warnings: 0
+	// Identified viruses: 1
+	// I/O errors: 0
+	lines := strings.Split(bitdefenderout, "\n")
+
+	// Extract Virus string
+	for _, line := range lines {
+		if len(line) != 0 {
+			switch {
+			case strings.Contains(line, "infected:"):
+				result := extractVirusName(line)
+				if len(result) != 0 {
+					bitdefender.Result = result
+					bitdefender.Infected = true
+				} else {
+					fmt.Println("[ERROR] Virus name extracted was empty: ", result)
+					os.Exit(2)
+				}
+			case strings.Contains(line, "Unices v"):
+				words := strings.Fields(line)
+				for _, word := range words {
+					if strings.HasPrefix(word, "v") {
+						bitdefender.Engine = strings.TrimPrefix(word, "v")
+					}
+				}
+			}
+		}
+	}
+
+	bitdefender.Updated = getUpdatedDate()
+
+	return bitdefender
+}
+
+// extractVirusName extracts Virus name from scan results string
+func extractVirusName(line string) string {
+	keyvalue := strings.Split(line, "infected:")
+	return strings.TrimSpace(keyvalue[1])
+}
+
+func printStatus(resp gorequest.Response, body string, errs []error) {
+	fmt.Println(resp.Status)
+}
+
+func getUpdatedDate() string {
+	if _, err := os.Stat("/opt/malice/UPDATED"); os.IsNotExist(err) {
+		return BuildTime
+	}
+	updated, err := ioutil.ReadFile("/opt/malice/UPDATED")
+	utils.Assert(err)
+	return string(updated)
+}
+
+func printMarkDownTable(bitdefender Bitdefender) {
+
+	fmt.Println("#### Bitdefender")
+	table := clitable.New([]string{"Infected", "Result", "Engine", "Updated"})
+	table.AddRow(map[string]interface{}{
+		"Infected": bitdefender.Results.Infected,
+		"Result":   bitdefender.Results.Result,
+		"Engine":   bitdefender.Results.Engine,
+		"Updated":  bitdefender.Results.Updated,
+	})
+	table.Markdown = true
+	table.Print()
+}
+
+func updateAV(ctx context.Context) error {
+	fmt.Println("Updating Bitdefender...")
+	fmt.Println(utils.RunCommand(ctx, "bdscan", "--update"))
+	// Update UPDATED file
+	t := time.Now().Format("20060102")
+	err := ioutil.WriteFile("/opt/malice/UPDATED", []byte(t), 0644)
+	return err
+}
+
+func main() {
+
+	var elastic string
+
+	cli.AppHelpTemplate = utils.AppHelpTemplate
+	app := cli.NewApp()
+
+	app.Name = "bitdefender"
+	app.Author = "blacktop"
+	app.Email = "https://github.com/blacktop"
+	app.Version = Version + ", BuildTime: " + BuildTime
+	app.Compiled, _ = time.Parse("20060102", BuildTime)
+	app.Usage = "Malice Bitdefender AntiVirus Plugin"
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "verbose, V",
+			Usage: "verbose output",
+		},
+		cli.BoolFlag{
+			Name:  "table, t",
+			Usage: "output as Markdown table",
+		},
+		cli.BoolFlag{
+			Name:   "post, p",
+			Usage:  "POST results to Malice webhook",
+			EnvVar: "MALICE_ENDPOINT",
+		},
+		cli.BoolFlag{
+			Name:   "proxy, x",
+			Usage:  "proxy settings for Malice webhook endpoint",
+			EnvVar: "MALICE_PROXY",
+		},
+		cli.StringFlag{
+			Name:        "elasitcsearch",
+			Value:       "",
+			Usage:       "elasitcsearch address for Malice to store results",
+			EnvVar:      "MALICE_ELASTICSEARCH",
+			Destination: &elastic,
+		},
+		cli.IntFlag{
+			Name:   "timeout",
+			Value:  10,
+			Usage:  "malice plugin timeout (in seconds)",
+			EnvVar: "MALICE_TIMEOUT",
+		},
+	}
+	app.Commands = []cli.Command{
+		{
+			Name:    "update",
+			Aliases: []string{"u"},
+			Usage:   "Update virus definitions",
+			Action: func(c *cli.Context) error {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Duration(c.Int("timeout"))*time.Second)
+				defer cancel()
+				return updateAV(ctx)
+			},
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(c.Int("timeout"))*time.Second)
+		defer cancel()
+
+		path, err := filepath.Abs(c.Args().First())
+		utils.Assert(err)
+
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			utils.Assert(err)
+		}
+		if c.Bool("verbose") {
+			log.SetLevel(log.DebugLevel)
+		}
+
+		bitdefender := Bitdefender{
+			Results: ParseBitdefenderOutput(utils.RunCommand(ctx, "bdscan", path)),
+		}
+
+		// upsert into Database
+		elasticsearch.InitElasticSearch(elastic)
+		elasticsearch.WritePluginResultsToDatabase(elasticsearch.PluginResults{
+			ID:       utils.Getopt("MALICE_SCANID", utils.GetSHA256(path)),
+			Name:     name,
+			Category: category,
+			Data:     structs.Map(bitdefender.Results),
+		})
+
+		if c.Bool("table") {
+			printMarkDownTable(bitdefender)
+		} else {
+			bitdefenderJSON, err := json.Marshal(bitdefender)
+			utils.Assert(err)
+			if c.Bool("post") {
+				request := gorequest.New()
+				if c.Bool("proxy") {
+					request = gorequest.New().Proxy(os.Getenv("MALICE_PROXY"))
+				}
+				request.Post(os.Getenv("MALICE_ENDPOINT")).
+					Set("X-Malice-ID", utils.Getopt("MALICE_SCANID", utils.GetSHA256(path))).
+					Send(string(bitdefenderJSON)).
+					End(printStatus)
+
+				return nil
+			}
+			fmt.Println(string(bitdefenderJSON))
+		}
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	utils.Assert(err)
+}
